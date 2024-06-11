bfa0e6b2e417f8c518204054f610bfb93927f045
vikasmishra0921 <157030541+vikasmishra0921@users.noreply.github.com>
2024-06-11 09:30:40 +0000

    changes in js2

--- js/script2.js
+++ js/script2.js
@@ -22,7 +22,7 @@ function convertSecondsToMinuteSeconds(seconds) {
 async function getsongs(folder) {
   currFolder = folder;
   // let a = await fetch(`https://192.168.0.109:5500/${folder}/`)
-  let a = await fetch(`/spotify-clone/${folder}/`)
+  let a = await fetch(`//spotify-clone/${folder}/`)
 
   // let a = await fetch(`/songs/`)
 
@@ -95,7 +95,7 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
   // let a = await fetch(`https://192.168.0.109:5500/songs/`)
-  let a = await fetch(`/spotify-clone/songs/`)
+  let a = await fetch(`//spotify-clone/songs/`)
   // let a = await fetch(`/songs/`) //Jab isko Host karenge tab httpss: wala use nahi kar sakte hai but aise chalane ke liye wahi use hoga
   let response = await a.text();
   let div = document.createElement("div")
@@ -118,7 +118,7 @@ async function displayAlbums() {
 
 
       //GEt the metadata of the folder
-      let a = await fetch(`/spotify-clone/songs/${folder}/info.json`)
+      let a = await fetch(`//spotify-clone/songs/${folder}/info.json`)
       let response = await a.json();
       // console.log(response)
       cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
